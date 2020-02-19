973cb0dabc40f809971a7e91ac886ea23c534f62
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 19:39:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
     pigeon_hash = Hash.new
     data.each do |color_gender_lives, inner_keys|
         inner_keys.each do |category_key, name_arrays|
-            name_arrays.each do |index_name|
+            name_arrays.each do |name|
                 pigeon_hash[index_name] = {color: [], gender: [], lives: []}
             end
         end
