f2d88320562148d7c32b9b367348b1f277b6c794
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 20:08:08 +0000

    Automatically backed up by Learn

---
+++ nyc_pigeon_organizer.rb
@@ -0,0 +1,39 @@
+def nyc_pigeon_organizer(data)
+    pigeon_hash = Hash.new
+    data.each do |cgl, inner_data|
+        inner_data.each do |category_key, name_arrays|
+            name_arrays.each do |name|
+                pigeon_hash[name] = {color: [], gender: [], lives: []}
+            end
+        end
+    end
+  option = pigeon_hash
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      option.each do |item|
+        if bird_name === item
+          pigeon_hash[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      option.each do |item|
+        if bird_name === item
+          pigeon_hash[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      option.each do |item|
+        if bird_name === item
+          pigeon_hash[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  return pigeon_hash
+end 
