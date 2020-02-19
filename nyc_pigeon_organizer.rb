7beff7924d97296417b3dc85c5fbf657a53a8109
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 20:06:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,40 +1,41 @@
-def nyc_pigeon_organizer(data)
-    pigeon_hash = Hash.new
-    data.each do |cgl, inner_data|
-        inner_data.each do |category_key, name_arrays|
-            name_arrays.each do |name|
-                pigeon_hash[name] = {color: [], gender: [], lives: []}
-            end
-        end
-    end
-  option = pigeon_hash
+def nyc_pigeon_organizer (data)
+  final = {}
+
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = final.keys
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
-      option.each do |item|
+      x.each do |item|
         if bird_name === item
-          pigeon_hash[item][:color] << bird_color.to_s
+          final[item][:color] << bird_color.to_s
         end 
       end 
     end 
   end 
   data[:gender].each do |gender, type|
     type.each do |bird_name|
-      option.each do |item|
+      x.each do |item|
         if bird_name === item
-          pigeon_hash[item][:gender] << gender.to_s
+          final[item][:gender] << gender.to_s
         end 
       end 
     end 
   end 
   data[:lives].each do |location, name|
     name.each do |bird_name|
-      option.each do |item|
+      x.each do |item|
         if bird_name === item
-          pigeon_hash[item][:lives] << location
+          final[item][:lives] << location
         end 
       end 
     end 
   end 
-  return pigeon_hash
+  
+  return final 
 end 
-
